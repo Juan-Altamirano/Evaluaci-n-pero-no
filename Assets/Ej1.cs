f3a8e498781ef33ec8e66626abc91bbf17203dbc
Juan-Altamirano <101400097+Juan-Altamirano@users.noreply.github.com>
2022-04-07 15:06:04 -0300

    Ejercicios 1 y 2 del simulacro
    lo del titulo

---
+++ Assets/Ej1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej1 : MonoBehaviour
+{
+    public int num1, num2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (num1 == num2)
+        {
+            Debug.Log("Los números " + num1 + " y " + num2 + " son iguales");
+        }
+
+        else
+        {
+            Debug.Log("Los números " + num1 + " y " + num2 + " no son iguales");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Ej2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej2 : MonoBehaviour
+{
+    public int precio1, precio2, precio3, dinero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int VTotal = precio1 + precio2 + precio3;
+        int Resto = dinero - VTotal;
+        int Falta = VTotal - dinero;
+        if (VTotal < dinero)
+        {
+            Debug.Log("La suma de dinero alcanza y sobra/n " + Resto + " peso/s");
+        }
+
+        else
+        {
+            Debug.Log("La suma de dinero no alcanza y falta/n " + Falta + " peso/s");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
